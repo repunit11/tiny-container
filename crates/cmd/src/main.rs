30a3e9068cf199b4a4c2d771abe65290bb1cfa85
kenta-kenta <kenken22352235@gmail.com>
2026-03-08 12:29:02 +0900

    サブコマンドを扱えるようにした

--- crates/cmd/src/main.rs
+++ crates/cmd/src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = "tiny-youki",
+    about = "tiny-youki (WIP) - Open Container Initiative runtime",
+    author = "n4mlz",
+    arg_required_else_help = true
+)]
+
+struct Cli {
+    #[clap(subcommand)]
+    command: SubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommands {
+    #[clap(name = "create", about = "create a container")]
+    Create {
+        /// container id
+        container_id: String,
+
+        /// bundle path
+        #[clap(short = 'b', long, value_name = "PATH", required = true)]
+        bundle: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: Cli = Cli::parse();
+
+    match &cli.command {
+        SubCommands::Create {
+            container_id,
+            bundle,
+        } => {
+            println!("container_id: {}", container_id);
+            println!("bundle: {:?}", bundle);
+        }
+    }
 }
--- docs/steps/02-cli-and-commands.md
+++ docs/steps/02-cli-and-commands.md
@@ -10,3 +10,29 @@
 
 - clapとは、Command Line Argument Parser for Rustの略であり、RustでCLIを作るときに利用する定番のクレートである
 - 導入方法は、 `cargo add clap --features derive (-p cmd)`
+
+ここでCLI全体のメタ設定を行う
+
+```rust
+#[clap(
+    name = "tiny-youki",
+    about = "tiny-youki (WIP) - Open Container Initiative runtime",
+    author = "n4mlz",
+    arg_required_else_help = true
+)]
+```
+
+ここでサブコマンドの引数の型の定義を行う。  
+コマンドの実装は別の部分で行う。
+
+```rust
+enum SubCommands {
+    #[clap(name = "create", about = "create a container")]
+    Create {
+        container_id: String,
+
+        #[clap(short = 'b', long, value_name = "PATH", required = true)]
+        bundle: PathBuf,
+    },
+}
+```
