e695cab4afc5dfe1d1dcf993d21ef33872ec3945
kenta-kenta <kenken22352235@gmail.com>
2026-03-11 13:09:26 +0900

    定数を定義した

---
+++ crates/libcontainer/src/constant.rs
@@ -0,0 +1,4 @@
+pub const BASE_PATH: &str = "/tmp/tiny-youki";
+pub const CONTAINER_PATH: &str = "containers";
+pub const NEWUIDMAP: &str = "/usr/bin/newuidmap";
+pub const NEWGIDMAP: &str = "/usr/bin/newgidmap";
--- crates/libcontainer/src/lib.rs
+++ crates/libcontainer/src/lib.rs
@@ -1,5 +1,7 @@
+mod constant;
 mod container;
 mod state;
 
+pub use constant::*;
 pub use container::*;
 pub use state::*;
